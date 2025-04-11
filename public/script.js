dc047bb19e68538ce2ae0ebeb388a5d94fe6ab56
Ben-Learner <qiben14zdq@gmail.com>
2025-04-11 20:49:14 +0800

    add information

--- public/script.js
+++ public/script.js
@@ -232,6 +232,22 @@
                     // 将 <a> 标签添加到 <h3> 标签中
                     el.appendChild(link);
                   } 
+
+                  else if (key === 'teamMember7Name') {
+                    // 特殊处理 teamMember6Name，添加链接
+                    const homepageUrl = 'https://www.researchgate.net/profile/Chenghao-Cao-2?ev=prf_overview';
+                    const link = document.createElement('a');
+                    link.href = homepageUrl;
+                    link.target = '_blank';
+                    link.rel = 'noopener noreferrer';
+                    link.className = 'text-gray-800 hover:underline';
+                    link.textContent = languageData[lang][key];
+    
+                    // 清空 <h3> 标签的内容
+                    el.innerHTML = '';
+                    // 将 <a> 标签添加到 <h3> 标签中
+                    el.appendChild(link);
+                  } 
                   
                   else if (key.startsWith('pub')) {
                     el.innerHTML = languageData[lang][key];
